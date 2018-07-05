0f9b09a3ef846d22aa4e5e902952393121fb46e7
iaortiz <38107740+iaortiz@users.noreply.github.com>
2018-07-05 12:23:28 -0500

    Agregando trabajo grupal 2

--- Trabajo-Grupal-2/src/trabajo/grupal/pkg2/Estudiante.java
+++ Trabajo-Grupal-2/src/trabajo/grupal/pkg2/Estudiante.java
@@ -10,19 +10,19 @@ package trabajo.grupal.pkg2;
  * @author Israel
  */
 public class Estudiante {
-
+// Declaración de variables globales
     String nombre;
     String apellido;
     int edad;
     double calificacion1;
     double calificacion2;
-
+// Método para presentar el reporte
     public void presentar() {
         System.out.printf("%s %s\nEdad: %d\nCalificacion 1: %.1f\nCalificacion 2: %.1f\nPromedio: %.1f\n\n", nombre, apellido, edad,
                 calificacion1, calificacion2, obtener_promedio());
 
     }
-
+// Método con la operación requerida para obtener el promedio
     public double obtener_promedio() {
         double promedio = 0;
         promedio = (calificacion1 + calificacion2) / 2;
